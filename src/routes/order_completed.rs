cf9e56a13a2e1342cb024cd92c4140186b29b485
benluelo <benluelo@hotmail.com>
2023-02-07 14:46:44 -0500

    update endpoint

--- src/routes/order_completed.rs
+++ src/routes/order_completed.rs
@@ -3,7 +3,6 @@ use std::sync::Arc;
 use axum::{
     extract::{Path, State},
     http::StatusCode,
-    response::IntoResponse,
 };
 use sqlx::{query, PgPool};
 use tokio::sync::watch;
@@ -15,7 +14,7 @@ pub(crate) async fn post(
     Path(order_number): Path<OrderNumber>,
     State(sender): State<Arc<watch::Sender<Breaks>>>,
     State(db): State<PgPool>,
-) -> impl IntoResponse {
+) -> StatusCode {
     sender.send_modify(|breaks| breaks.remove_by_id(order_number));
 
     match query!(
@@ -30,15 +29,12 @@ pub(crate) async fn post(
     {
         Ok(_) => {
             tracing::info!("successfully deleted order #{}", &order_number);
-            StatusCode::OK.into_response()
+            StatusCode::OK
         }
         Err(why) => {
             tracing::error!("error deleting from the database: {}", &why);
-            (
-                StatusCode::INTERNAL_SERVER_ERROR
-                // Json(format!("error deleting from the database: {}", why)),
-            )
-                .into_response()
+
+            StatusCode::INTERNAL_SERVER_ERROR
         }
     }
 }
